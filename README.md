c4ac25b2ff9cba428d85b3c5611cd97af8a21665
zheng <yootaoo@gmail.com>
2018-06-10 01:44:41 +0200

    update README.md

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
 # AsciiArt
 
-![](https://raw.githubusercontent.com/zhoudaxia233/AsciiArt/develop/cover.png)
+<img src="https://raw.githubusercontent.com/zhoudaxia233/AsciiArt/develop/cover.png" width="256">
+
 - [x] Asciify an image into `txt` & `html`
 - [ ] Asciify many images parallelly
 - [ ] Asciify a GIF
 - [ ] Asciify a video
 
+---
+## Requirements
+1. `Python3`
+2. `Pillow-5.1.0` (Only this version has been tested, other versions may also work)
+
+---
+
+## Usage
+```
+./main.py avatar.png
+```
+You can use `--scaling_ratio` to specify the scaling ratio of the output with respect to the input image. The default value is `0.5`.  
+```
+./main.py avatar.png --scaling_ratio 0.1
+```
+
+You can also use `--output_format` to specify the format of the output file.  
+>`0` represents for `.txt` format  
+`1` represents for `.html` format  
+`2` represents for both `.txt` format & `.html` format
+
+The default value is `2`.
+```
+./main.py avatar.png --scaling_ratio 0.1 --output_format 1
+```
--- main.py
+++ main.py
@@ -7,19 +7,19 @@ def init():
     parser = argparse.ArgumentParser()
     parser.add_argument('file')
     parser.add_argument('--scaling_ratio', type=float, default=0.5)
-    parser.add_argument('--output_type', type=int, default=2)
+    parser.add_argument('--output_format', type=int, default=2)
     args = parser.parse_args()
-    return (args.file, args.scaling_ratio, args.output_type)
+    return (args.file, args.scaling_ratio, args.output_format)
 
 def main():
-    img_path, scaling_ratio, output_type = init()
+    img_path, scaling_ratio, output_format = init()
     filename = img_path.split('.')[0]
     asciiart = AsciiArt(img_path)
-    if output_type == 0:
+    if output_format == 0:
         asciiart.save_as_txt_file(scaling_ratio, filename)
-    elif output_type == 1:
+    elif output_format == 1:
         asciiart.save_as_html_file(scaling_ratio, filename)
-    elif output_type == 2:
+    elif output_format == 2:
         asciiart.save_as_txt_file(scaling_ratio, filename)
         asciiart.save_as_html_file(scaling_ratio, filename)
     else:
